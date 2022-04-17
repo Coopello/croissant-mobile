5ae7514985d30881ca80176b27ae1dcab13b61fa
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2022-04-17 23:22:26 +0900

    feat: create User class

---
+++ shared/src/commonMain/kotlin/com/coopelife/croissant/data/entitiy/User.kt
@@ -0,0 +1,8 @@
+package com.coopelife.croissant.data.entitiy
+
+class User(
+    val id: Int,
+    val name: String,
+    val proposedPlanList: List<Int>,
+    val participatedPlanLIst: List<Int>
+)
