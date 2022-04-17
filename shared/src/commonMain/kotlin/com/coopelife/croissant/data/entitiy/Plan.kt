e6519394bd5027b0c83caec7fcf878ce6f6a8ad4
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2022-04-17 23:19:58 +0900

    feat: create Plan class

---
+++ shared/src/commonMain/kotlin/com/coopelife/croissant/data/entitiy/Plan.kt
@@ -0,0 +1,16 @@
+package com.coopelife.croissant.data.entitiy
+
+class Plan(
+    val id: Int,
+    val shopName: String,
+    val maxNumberOfPeople: Int,
+    val minNumberOfPeople: Int?,
+    val proposerId: Int,
+    val participantIds: List<Int>,
+    val meetingTime: Long,
+    val status: PlanStatus
+)
+
+enum class PlanStatus(val code: Int) {
+    NOT_ESTABLISHED(0), ESTABLISHED(1), FINISHED(2)
+}
