24139456ee11cc5ae9387fc5cf0043f84c751d64
Kaito-Dogi <kendobu0405@gmail.com>
2022-04-20 02:53:40 +0900

    feat: jetpack compose no の設定完了

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -17,11 +17,27 @@ android {
             isMinifyEnabled = false
         }
     }
+    buildFeatures {
+        compose = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.1.1"
+    }
 }
 
 dependencies {
     implementation(project(":shared"))
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+    implementation("androidx.activity:activity-compose:1.4.0")
+    implementation("androidx.compose.material:material:1.1.1")
+    implementation("androidx.compose.animation:animation:1.1.1")
+    implementation("androidx.compose.ui:ui-tooling:1.1.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.1")
+    implementation("androidx.compose.ui:ui-test-junit4:1.1.1")
 }
--- androidApp/src/main/java/com/coopelife/croissant/android/MainActivity.kt
+++ androidApp/src/main/java/com/coopelife/croissant/android/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.coopelife.croissant.android
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            Text(text = "Hello. World!")
+        }
     }
 }
