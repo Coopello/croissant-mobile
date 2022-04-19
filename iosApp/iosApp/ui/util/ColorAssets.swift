5d22a7a0c082a94bed294548f26ef7961d0897e2
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2022-04-19 21:05:16 +0900

    feat: add ColorAssets.swift

---
+++ iosApp/iosApp/ui/util/ColorAssets.swift
@@ -0,0 +1,12 @@
+//
+// Created by Kohei Inoue on 2022/04/19.
+// Copyright (c) 2022 orgName. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+extension UIColor {
+    static let primaryOrange = UIColor(named: "primary_orange")
+}
+
