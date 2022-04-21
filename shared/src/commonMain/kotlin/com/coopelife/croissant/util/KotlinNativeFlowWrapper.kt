b42c68aff69d9ba0a39c58a9885b6281ffb78e29
Kaito-Dogi <kendobu0405@gmail.com>
2022-04-22 02:11:12 +0900

    feat: iosScope を記述

--- shared/src/commonMain/kotlin/com/coopelife/croissant/util/KotlinNativeFlowWrapper.kt
+++ shared/src/commonMain/kotlin/com/coopelife/croissant/util/KotlinNativeFlowWrapper.kt
@@ -1,11 +1,14 @@
 package com.coopelife.croissant.util
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onEach
+import kotlin.coroutines.CoroutineContext
 
 class KotlinNativeFlowWrapper<T>(private val flow: Flow<T>) {
     fun subscribe(
@@ -18,4 +21,9 @@ class KotlinNativeFlowWrapper<T>(private val flow: Flow<T>) {
         .catch { onThrow(it) }
         .onCompletion { onComplete() }
         .launchIn(scope)
+}
+
+val iosScope: CoroutineScope = object : CoroutineScope {
+    override val coroutineContext: CoroutineContext
+        get() = SupervisorJob() + Dispatchers.Main
 }
