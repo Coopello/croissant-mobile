a9a2ccb141e1cf8f1c772311eb17a2ef0c34be28
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2022-04-25 23:47:13 +0900

    feat: HomeScreenで日付のセルをクリックするとそれが選択された状態になる仕様実装

--- iosApp/iosApp/ui/screen/home/HomeScreen.swift
+++ iosApp/iosApp/ui/screen/home/HomeScreen.swift
@@ -5,19 +5,52 @@
 
 import Foundation
 import SwiftUI
+import shared
 
 struct HomeScreen: View {
+    private let planRepository: PlanRepository
+    private let fetchRecentPlansUseCase: FetchRecentPlansUseCase
+    
+    @ObservedObject var viewModel: HomeScreenViewModel
+    
+    init() {
+        planRepository = FakePlanRepository()
+        fetchRecentPlansUseCase = FetchRecentPlansUseCase(
+            planRepository: planRepository
+        )
+        viewModel = HomeScreenViewModel(
+            fetchRecentPlansUseCase: fetchRecentPlansUseCase
+        )
+    }
+    
+    private let dateArray: [String] = ["7/16", "7/17", "7/18", "7/19", "7/20"]
+    
     var body: some View {
         GeometryReader { metrics in
             VStack {
                 HStack {
-                    DateCircleSelected(dateText: "7/16")
-                    DateCircleUnselected(dateText: "7/17")
-                    DateCircleUnselected(dateText: "7/18")
-                    DateCircleUnselected(dateText: "7/19")
-                    DateCircleUnselected(dateText: "7/20")
+                    ForEach(0..<dateArray.count) { index in
+                        if index == viewModel.state.howManyDaysLaterIsBeingClicked {
+                            DateCircleSelected(dateText: dateArray[index]).onTapGesture {
+                                viewModel.onTriggerEvent(
+                                    event: HomeScreenEvent.ClickDate(
+                                        howManyDaysLater: Int32(index)
+                                    )
+                                )
+                            }
+                        } else {
+                            DateCircleUnselected(dateText: dateArray[index]).onTapGesture {
+                                viewModel.onTriggerEvent(
+                                    event: HomeScreenEvent.ClickDate(
+                                        howManyDaysLater: Int32(index)
+                                    )
+                                )
+                            }
+                        }
+                    }
                 }
                 .frame(height: metrics.size.height * 0.15)
+                
                 Spacer()
             }
         }
