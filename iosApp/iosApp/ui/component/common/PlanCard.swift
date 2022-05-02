c2f3f0d6f2aff5912362fb74cb8ea034b420c12b
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2022-05-02 12:28:03 +0900

    feat: PlanCardを角丸にしてstrokeをつけた

--- iosApp/iosApp/ui/component/common/PlanCard.swift
+++ iosApp/iosApp/ui/component/common/PlanCard.swift
@@ -8,17 +8,52 @@ import SwiftUI
 import shared
 
 struct PlanCard: View {
-   private let plan: Plan
-
-   init(plan: Plan) {
-       self.plan = plan
-   }
+//   private let plan: Plan
+//
+//   init(plan: Plan) {
+//       self.plan = plan
+//   }
     
     var body: some View {
-        HStack {
-            TextWithTitle(
-                title: MainActivityString.destinationShop,
-                description: "Maru"
+        GeometryReader { metrics in
+            VStack {
+                TextWithTitle(
+                    title: MainActivityString.destinationShop,
+                    description: "Maru"
+                )
+                Spacer()
+                TextWithTitle(
+                    title: MainActivityString.meetingPlace,
+                    description: "オフィスの下"
+                )
+                Spacer()
+                TextWithTitle(
+                    title: MainActivityString.meetingTime,
+                    description: "14:00"
+                )
+                Spacer()
+                TextWithTitle(
+                    title: MainActivityString.theTimeRequired,
+                    description: "1時間(目安)"
+                )
+                Spacer()
+                TextWithTitle(
+                    title: MainActivityString.peopleNumbers,
+                    description: "4人/3人"
+                )
+            }
+            .padding()
+            .frame(
+                width: metrics.size.width * 0.6,
+                height: metrics.size.height * 0.6,
+                alignment: .center
+            )
+            .overlay(
+                RoundedRectangle(cornerRadius: 8)
+                    .stroke(
+                        Color(Colors.primaryGray.name),
+                        lineWidth: 2
+                    )
             )
         }
     }
