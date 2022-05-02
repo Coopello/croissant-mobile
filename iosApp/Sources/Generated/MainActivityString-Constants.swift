8388ef20ad5b111885b15e561848375af9b9854c
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2022-05-02 13:57:03 +0900

    feat: MainActivityString追加

--- iosApp/Sources/Generated/MainActivityString-Constants.swift
+++ iosApp/Sources/Generated/MainActivityString-Constants.swift
@@ -22,6 +22,8 @@ internal enum MainActivityString {
   internal static let meetingTime = MainActivityString.tr("mainactivity", "meeting_time")
   /// マイページ
   internal static let mypage = MainActivityString.tr("mainactivity", "mypage")
+  /// 参加する
+  internal static let participate = MainActivityString.tr("mainactivity", "participate")
   /// 最低人数/最高人数
   internal static let peopleNumbers = MainActivityString.tr("mainactivity", "people_numbers")
   /// 募集一覧
