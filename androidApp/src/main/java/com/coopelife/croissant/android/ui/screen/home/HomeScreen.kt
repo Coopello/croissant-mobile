760784811090b43c022d33ff087a87593167b2c2
Kaito Dogi <kendobu0405@gmail.com>
2022-05-15 23:58:02 +0900

    feat: HomeScreenの実装

--- androidApp/src/main/java/com/coopelife/croissant/android/ui/screen/home/HomeScreen.kt
+++ androidApp/src/main/java/com/coopelife/croissant/android/ui/screen/home/HomeScreen.kt
@@ -1,4 +1,17 @@
 package com.coopelife.croissant.android.ui.screen.home
 
-class HomeScreen {
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+
+@Composable
+fun HomeScreen(
+    nacController: NavController,
+    homeScreenViewModel: HomeScreenViewModel = viewModel()
+) {
+    val screenName: String by homeScreenViewModel.screenName.observeAsState("")
+    Text(text = screenName)
 }
