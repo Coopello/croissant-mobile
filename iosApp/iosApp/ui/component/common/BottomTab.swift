8d75b02c098c55451c2ebfd99349c762656f84d9
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2022-05-21 21:42:48 +0900

    fix/BottomTabのプラスボタンを押した時には画面更新を走らせないように修正

--- iosApp/iosApp/ui/component/common/BottomTab.swift
+++ iosApp/iosApp/ui/component/common/BottomTab.swift
@@ -18,55 +18,101 @@ struct BottomTab: View {
         self.metrics = metrics
         self.onClickTab = onClickTab
     }
-
-    let itemDictionary: [(key: Int, value: (String, String))] = [
-        0: (MainActivityString.imagePathOfWallet, MainActivityString.planList),
-        1: (Images.bottomTabButton.name, String()),
-        2: (MainActivityString.imagePathOfPerson, MainActivityString.mypage)
-    ].sorted {
-        $0.key < $1.key
-    }
-
+    
+    private let tabItemContents: [TabItemContent] = [
+        TabItemContent(
+            imagePath: MainActivityString.imagePathOfWallet,
+            text: MainActivityString.planList
+        ),
+        TabItemContent(
+            imagePath: MainActivityString.imagePathOfPerson,
+            text: MainActivityString.mypage
+        )
+    ]
+    
     var body: some View {
-        let imageNames = itemDictionary.map {
-            $0.value.0
-        }
-        let texts = itemDictionary.map {
-            $0.value.1
-        }
-
         VStack {
             Spacer()
             HStack {
-                ForEach(imageNames.indices) { index in
-                    // FIXME: ここをクリックリスナーに差し替える
-                    Button(action: {
-                        onClickTab(Int8(index))
-                        selectedIndex = Int8(index)
-                    }) {
-                        Spacer()
-                        VStack {
-                            if !texts[index].isEmpty {
-                                Image(systemName: imageNames[index])
-                                        .renderingMode(.template)
-                                        .resizable()
-                                        .foregroundColor(.white)
-                                        .frame(width: 28, height: 28)
-                                Text(texts[index])
-                                    .modifier(ExtraSmallText(textColor: .white))
-
-                            } else {
-                                Image(imageNames[index])
-                            }
-                        }
-                        Spacer()
-                    }.padding()
-                }
+                TabItem(
+                    content: TabItemContent(
+                        imagePath: MainActivityString.imagePathOfWallet,
+                        text: MainActivityString.planList
+                    ),
+                    index: 0,
+                    onClick: { index in
+                        selectedIndex = index
+                        onClickTab(index)
+                    }
+                )
+                .padding()
+                
+                Image(Images.bottomTabButton.name).padding()
+                
+                TabItem(
+                    content: TabItemContent(
+                        imagePath: MainActivityString.imagePathOfPerson,
+                        text: MainActivityString.mypage
+                    ),
+                    index: 1,
+                    onClick: { index in
+                        selectedIndex = index
+                        onClickTab(index)
+                    }
+                )
+                .padding()
             }.background(Color(Colors.primaryOrange.name))
         }
         .frame(
             maxWidth: .infinity,
             maxHeight: metrics.size.height * 0.1
         )
+    }
+}
+
+private struct TabItemContent {
+    let imagePath: String
+    let text: String
+    
+    init(
+        imagePath: String,
+        text: String
+    ) {
+        self.imagePath = imagePath
+        self.text = text
+    }
+}
+
+private struct TabItem: View {
+    private let content: TabItemContent
+    private let index: Int8
+    private let onClick: (Int8) -> Void
+    
+    init(
+        content: TabItemContent,
+        index: Int8,
+        onClick: @escaping (Int8) -> Void
+    ) {
+        self.content = content
+        self.index = index
+        self.onClick = onClick
+    }
+    
+    var body: some View {
+        Button(action: {
+            onClick(index)
+        }) {
+            Spacer()
+            VStack {
+                Image(systemName: content.imagePath)
+                        .renderingMode(.template)
+                        .resizable()
+                        .foregroundColor(.white)
+                        .frame(width: 28, height: 28)
+                Text(content.text)
+                    .modifier(ExtraSmallText(textColor: .white))
+            }
+            Spacer()
+        }
     }
 }
