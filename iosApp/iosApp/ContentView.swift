990f16f59ac40200d85eb16919a2535ac5097829
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2022-05-24 09:41:36 +0900

    fix: ContentViewで直接MyPagePlanCellを呼び出している部分を修正

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -3,24 +3,7 @@ import shared
 
 struct ContentView: View {
 	var body: some View {
-//        HomeActivity()
-        GeometryReader { metrics in
-            let mockPlan = Plan(
-                id: 0,
-                shopName: "Maru",
-                maxNumberOfPeople: 9,
-                minNumberOfPeople: 8,
-                proposerId: 0,
-                participantIds: [0, 9, 8, 9, 9, 9],
-                meetingTime: 1653184751,
-                status: Plan.PlanStatus.established,
-                meetingPlace: ""
-            )
-            MyPagePlanCell(
-                plan: mockPlan,
-                metrics: metrics
-            )
-        }
+        HomeActivity()
 	}
 }
 
