f8490e64661f0a62f690cd484cceb8808a5d6c11
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2022-05-26 07:40:14 +0900

    feat: Tabを切り替えるレイアウトを作成

--- iosApp/iosApp/ui/component/mypagescreen/MyPageHeader.swift
+++ iosApp/iosApp/ui/component/mypagescreen/MyPageHeader.swift
@@ -4,6 +4,22 @@ import shared
 struct MyPageHeader: View {
     private let metrics: GeometryProxy
     private let user: User
+    @State var selectedTabIndex: Int = 0
+    
+    private let tabList = [
+        Tab(
+            text: MainActivityString.unformed,
+            type: TabType.unFormed
+        ),
+        Tab(
+            text: MainActivityString.formed,
+            type: TabType.formed
+        ),
+        Tab(
+            text: MainActivityString.participationHistory,
+            type: TabType.participationHistory
+        )
+    ]
     
     init(
         metrics: GeometryProxy,
@@ -30,19 +46,132 @@ struct MyPageHeader: View {
                 .padding()
             }.frame(
                 maxWidth: .infinity,
-                maxHeight: metrics.size.height * 0.12
-            ).background(
-                Color(Colors.primaryOrange.name)
+                maxHeight: metrics.size.height * 0.12,
+                alignment: .top
+            )
+
+            TabLayout(
+                tabList: tabList,
+                metrics: metrics,
+                selectedTabIndex: $selectedTabIndex,
+                onTabClicked: { index in
+                    self.selectedTabIndex = index
+                }
+            )
+            .frame(
+                maxWidth: .infinity,
+                maxHeight: metrics.size.height * 0.8,
+                alignment: .bottom
             )
         }
+        .frame(
+            maxWidth: .infinity,
+            maxHeight: metrics.size.height * 0.2
+        )
+        .background(
+            Color(Colors.primaryOrange.name)
+        )
+        .shadow(
+            color: Color(Colors.primaryGray.name),
+            radius: 4,
+            x: 0,
+            y: 4
+        )
     }
 }
 
-private struct TabItem {
+private enum TabType {
+    case formed
+    case unFormed
+    case participationHistory
+}
+
+private struct Tab: Equatable {
     let text: String
+    let type: TabType
+    let id: Int = UUID().uuidString.hash
     
-    init(text: String) {
+    init(
+        text: String,
+        type: TabType
+    ) {
         self.text = text
+        self.type = type
+    }
+    
+    static func == (lhs: Tab, rhs: Tab) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
+
+private struct TabItem: View {
+    private let tab: Tab
+    private let metrics: GeometryProxy
+    private let onClickTab: () -> Void
+    
+    init(
+        tab: Tab,
+        metrics: GeometryProxy,
+        onClickTab: @escaping () -> Void
+    ) {
+        self.tab = tab
+        self.metrics = metrics
+        self.onClickTab = onClickTab
+    }
+    
+    var body: some View {
+        Button(
+            action: {
+                onClickTab()
+            },
+            label: {
+                Text(tab.text).modifier(MediumText(textColor: .white))
+            }
+        )
+    }
+}
+
+private struct TabLayout: View {
+    private let tabList: [Tab]
+    private let metrics: GeometryProxy
+    @Binding var selectedTabIndex: Int
+    private let onTabClicked: (Int) -> Void
+    
+    init(
+        tabList: [Tab],
+        metrics: GeometryProxy,
+        selectedTabIndex: Binding<Int>,
+        onTabClicked: @escaping (Int) -> Void
+    ) {
+        self.tabList = tabList
+        self.metrics = metrics
+        self._selectedTabIndex = selectedTabIndex
+        self.onTabClicked = onTabClicked
+    }
+    
+    var body: some View {
+        HStack {
+            ForEach(tabList, id: \.id) { tab in
+                VStack {
+                    let currentIndex = tabList.firstIndex(of: tab)!
+                    
+                    TabItem(
+                        tab: tab,
+                        metrics: metrics
+                    ) {
+                        onTabClicked(currentIndex)
+                    }
+                    
+                    Rectangle()
+                        .fill(
+                            selectedTabIndex == currentIndex ? .white : .clear
+                        )
+                        .frame(
+                            height: 4
+                        )
+                }
+            }
+        }
     }
 }
 
