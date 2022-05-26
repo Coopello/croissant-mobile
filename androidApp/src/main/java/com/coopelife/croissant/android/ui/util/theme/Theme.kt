a1a130ff5437975a47260528567107c4c5fd4ed8
Kaito Dogi <kendobu0405@gmail.com>
2022-05-27 03:25:12 +0900

    feat: Theme.ktを実装

--- androidApp/src/main/java/com/coopelife/croissant/android/ui/util/theme/Theme.kt
+++ androidApp/src/main/java/com/coopelife/croissant/android/ui/util/theme/Theme.kt
@@ -1,4 +1,39 @@
 package com.coopelife.croissant.android.ui.util.theme
 
-class Theme {
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val LightColors = lightColors(
+    primary = Orange,
+    primaryVariant = OrangeDark,
+    onPrimary = Color.Black,
+    secondary = Pink,
+    secondaryVariant = PinkDark,
+    onSecondary = Color.Black,
+    error = OrangeLight,
+)
+
+// FIXME: ダークテーマを定義する。
+private val DarkColors = lightColors(
+    primary = Orange,
+    primaryVariant = OrangeDark,
+    onPrimary = Color.Black,
+    secondary = Pink,
+    secondaryVariant = PinkDark,
+    onSecondary = Color.Black,
+    error = OrangeLight,
+)
+
+@Composable
+fun CroissantTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colors = if (darkTheme) DarkColors else LightColors,
+        content = content,
+    )
 }
