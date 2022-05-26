fc97fff109cadaef3767135b705e2924a8c6d045
Kaito Dogi <kendobu0405@gmail.com>
2022-05-27 05:30:18 +0900

    feat: 試しに円を描画

--- androidApp/src/main/java/com/coopelife/croissant/android/ui/component/home/DateSelectRadioGroup.kt
+++ androidApp/src/main/java/com/coopelife/croissant/android/ui/component/home/DateSelectRadioGroup.kt
@@ -1,7 +1,29 @@
 package com.coopelife.croissant.android.ui.component.home
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun DataSelectRadioGroup() {
+fun DateSelectRadioGroup() {
+}
+
+@Composable
+fun DateSelectRadioButton() {
+    Box(
+        modifier = Modifier
+            .size(100.dp)
+            .clip(shape = CircleShape)
+    )
+}
+
+@Preview
+@Composable
+fun Preview() {
+    DateSelectRadioButton()
 }
