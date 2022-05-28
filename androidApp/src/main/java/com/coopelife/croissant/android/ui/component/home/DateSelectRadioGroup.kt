725ba31d8508cc1f916479ad9e8703f7a3fc433c
Kaito Dogi <kendobu0405@gmail.com>
2022-05-29 03:00:16 +0900

    feat: DateSelectedRadioButtonを実装

--- androidApp/src/main/java/com/coopelife/croissant/android/ui/component/home/DateSelectRadioGroup.kt
+++ androidApp/src/main/java/com/coopelife/croissant/android/ui/component/home/DateSelectRadioGroup.kt
@@ -1,29 +1,84 @@
 package com.coopelife.croissant.android.ui.component.home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.coopelife.croissant.android.ui.util.theme.Orange
 
 @Composable
 fun DateSelectRadioGroup() {
 }
 
 @Composable
-fun DateSelectRadioButton() {
-    Box(
-        modifier = Modifier
-            .size(100.dp)
-            .clip(shape = CircleShape)
-    )
+fun DateSelectRadioButton(
+    isSelected: Boolean,
+    onClick: () -> Unit,
+    dateText: String,
+) {
+    if (isSelected)
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .clip(shape = CircleShape)
+                .clickable { onClick() }
+                .background(Orange),
+            contentAlignment = Alignment.Center,
+        ) {
+            Text(
+                text = dateText,
+                color = Color.White,
+                style = MaterialTheme.typography.h4,
+                textAlign = TextAlign.Center,
+            )
+        }
+    else
+        Box(
+            modifier = Modifier
+                .size(80.dp)
+                .clip(shape = CircleShape)
+                .clickable { onClick() }
+                .border(
+                    width = 4.dp,
+                    color = Orange,
+                    shape = RoundedCornerShape(40.dp)
+                )
+                .background(
+                    color = Color.White,
+                    shape = RoundedCornerShape(40.dp)
+                ),
+            contentAlignment = Alignment.Center,
+        ) {
+            Text(
+                text = dateText,
+                color = Color.Black,
+                style = MaterialTheme.typography.h5,
+                textAlign = TextAlign.Center,
+            )
+        }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun Preview() {
-    DateSelectRadioButton()
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        DateSelectRadioButton(isSelected = true, dateText = "5/28", onClick = {})
+        DateSelectRadioButton(isSelected = false, dateText = "5/29", onClick = {})
+    }
 }
