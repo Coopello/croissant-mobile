373f78eb6ea99739c42eaa50e35a206a0bce92f5
Kaito Dogi <kendobu0405@gmail.com>
2022-06-03 01:50:14 +0900

    feat: indicatorを実装

--- androidApp/src/main/java/com/coopelife/croissant/android/ui/component/home/HomeTabLayout.kt
+++ androidApp/src/main/java/com/coopelife/croissant/android/ui/component/home/HomeTabLayout.kt
@@ -1,25 +1,41 @@
 package com.coopelife.croissant.android.ui.component.home
 
-import androidx.compose.material.Tab
-import androidx.compose.material.TabRow
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.coopelife.croissant.android.R
+import com.coopelife.croissant.android.ui.util.theme.Orange
 
 @Composable
-fun HomeTabLayout() {
+fun HomeTabLayout(modifier: Modifier = Modifier) {
     var tabIndex by rememberSaveable { mutableStateOf(0) }
     val tabTitleList: List<String> = listOf("未成立", "成立済")
-    TabRow(selectedTabIndex = tabIndex) {
-        tabTitleList.forEachIndexed() { index: Int, title: String ->
-            Tab(
-                selected = index == tabIndex,
-                onClick = { tabIndex = index },
-                text = { Text(text = title) }
-            )
-        }
-    }
+}
+
+@Composable
+private fun EllipseIndicator() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(dimensionResource(R.dimen.ellipse_indicator_height))
+            .clip(shape = RoundedCornerShape(dimensionResource(R.dimen.ellipse_indicator_corner_radius)))
+            .background(Orange)
+    )
+}
+
+@Preview
+@Composable
+private fun TabLayoutPreview() {
+    EllipseIndicator()
 }
