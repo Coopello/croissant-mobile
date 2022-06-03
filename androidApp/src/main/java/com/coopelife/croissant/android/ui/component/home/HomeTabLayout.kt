d8f6f8d7f79b66365163f2a20009524cd9ce8984
Kaito Dogi <kendobu0405@gmail.com>
2022-06-04 05:04:26 +0900

    fix: 余分なSpacerを削除

--- androidApp/src/main/java/com/coopelife/croissant/android/ui/component/home/HomeTabLayout.kt
+++ androidApp/src/main/java/com/coopelife/croissant/android/ui/component/home/HomeTabLayout.kt
@@ -2,18 +2,20 @@ package com.coopelife.croissant.android.ui.component.home
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -21,6 +23,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.style.TextAlign
 import com.coopelife.croissant.android.R
 import com.coopelife.croissant.android.ui.util.theme.Orange
 import com.coopelife.croissant.android.ui.util.theme.OrangeLight
@@ -41,18 +44,22 @@ fun HomeTabLayout(modifier: Modifier = Modifier) {
             modifier = Modifier.fillMaxWidth()
         ) {
             tabTitleList.forEachIndexed { index: Int, tabTitle: String ->
-                Spacer(modifier = Modifier.weight(1f))
                 Text(
                     text = tabTitle,
                     style = MaterialTheme.typography.h5,
                     color = Color.White,
-                    modifier = Modifier.clickable(
-                        onClick = {
-                            tabIndex = index
-                        },
-                    )
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .weight(1f)
+                        .clickable(
+                            // FIXME: Ripple を変更する
+                            indication = rememberRipple(radius = dimensionResource(R.dimen.padding_0dp)),
+                            interactionSource = remember { MutableInteractionSource() },
+                            onClick = {
+                                tabIndex = index
+                            },
+                        )
                 )
-                Spacer(modifier = Modifier.weight(1f))
             }
         }
     }
