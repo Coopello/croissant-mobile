5e9476302662eff72de00d550a79c6045135f563
Kaito Dogi <kendobu0405@gmail.com>
2022-06-28 04:58:54 +0900

    feat: AppBar の実装

--- androidApp/src/main/java/com/coopelife/croissant/android/ui/screen/mypage/MypageScreen.kt
+++ androidApp/src/main/java/com/coopelife/croissant/android/ui/screen/mypage/MypageScreen.kt
@@ -1,10 +1,12 @@
 package com.coopelife.croissant.android.ui.screen.mypage
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.coopelife.croissant.android.ui.component.mypage.MypageAppBar
 import com.coopelife.croissant.android.ui.component.mypage.TabLayout
 
 @Composable
@@ -19,5 +21,11 @@ fun MypageScreen(
 
 @Composable
 fun MypageContent(previewText: String, onValueChanged: (String) -> Unit) {
-    TabLayout()
+    Column {
+        MypageAppBar(
+            userName = "秘密結社こぺろ",
+            userImageUrl = "https://lohas.nicoseiga.jp/thumb/946062i"
+        )
+        TabLayout()
+    }
 }
