cfe0eb3799ecd3ab2cb556484e8a01e329450bf7
Kaito Dogi <kendobu0405@gmail.com>
2022-06-29 01:49:34 +0900

    feat: 必要な components を宣言

---
+++ androidApp/src/main/java/com/coopelife/croissant/android/ui/component/mypage/PlanRow.kt
@@ -0,0 +1,18 @@
+package com.coopelife.croissant.android.ui.component.mypage
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+internal fun PlanRow(
+    modifier: Modifier = Modifier
+) {
+}
+
+@Composable
+private fun PlanCard() {
+}
+
+@Composable
+private fun DayOfWeekAndDayOfMonth() {
+}
