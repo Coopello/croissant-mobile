7324d997b7033f4cf2738ef4aa7f2596a4f12feb
Kaito Dogi <kendobu0405@gmail.com>
2022-07-09 21:15:09 +0900

    fix: HomeScreenEvent を追加・修正

--- shared/src/commonMain/kotlin/com/coopelife/croissant/ui/screen/home/HomeScreenEvent.kt
+++ shared/src/commonMain/kotlin/com/coopelife/croissant/ui/screen/home/HomeScreenEvent.kt
@@ -3,7 +3,26 @@ package com.coopelife.croissant.ui.screen.home
 import com.coopelife.croissant.data.entitiy.Plan
 
 sealed class HomeScreenEvent {
-    class ClickDate(val howManyDaysLater: Int) : HomeScreenEvent()
+    class OnInit(
+        val tabIndex: Int
+    ) : HomeScreenEvent()
+
+    class OnClickDate(
+        val howManyDaysLater: Int
+    ) : HomeScreenEvent()
+
+    class OnUpdatePlans(
+        val newPlans: List<Plan>
+    ) : HomeScreenEvent()
+
+    object OnUnFormedTabSelected : HomeScreenEvent()
+
+    object OnFormedTabSelected : HomeScreenEvent()
+
+    class OnClickParticipateButton(
+        val planId: Int,
+        val userId: Int,
+    ) : HomeScreenEvent()
+
     object DoNothing : HomeScreenEvent()
-    class UpdatePlans(val newPlans: List<Plan>) : HomeScreenEvent()
 }
