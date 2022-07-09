b166b29a01927fd1e3e3b55ffd3f3f55a5485930
Kaito Dogi <kendobu0405@gmail.com>
2022-07-09 19:34:32 +0900

    fix: shared モジュールのファイルを commit

--- shared/src/androidMain/kotlin/com/coopelife/croissant/domain/usecase/FetchRecentPlansUseCase.kt
+++ shared/src/androidMain/kotlin/com/coopelife/croissant/domain/usecase/FetchRecentPlansUseCase.kt
@@ -1,5 +1,10 @@
 package com.coopelife.croissant.domain.usecase
 
+import com.coopelife.croissant.data.entitiy.Plan
 import com.coopelife.croissant.data.repository.PlanRepository
 
-actual class FetchRecentPlansUseCase actual constructor(planRepository: PlanRepository)
+actual class FetchRecentPlansUseCase actual constructor(
+    private val planRepository: PlanRepository
+) {
+    suspend fun fetchMyPlans(): List<Plan> = planRepository.fetchRecentPlans()
+}
