6f412e26c4174ab82912d244e1cdc1a17ae79de6
Kaito Dogi <kendobu0405@gmail.com>
2022-07-09 20:29:42 +0900

    feat: モックデータの流し込みの実装

--- androidApp/src/main/java/com/coopelife/croissant/android/ui/screen/home/HomeScreen.kt
+++ androidApp/src/main/java/com/coopelife/croissant/android/ui/screen/home/HomeScreen.kt
@@ -2,9 +2,10 @@ package com.coopelife.croissant.android.ui.screen.home
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import com.coopelife.croissant.android.ui.mockPlanList
+import com.coopelife.croissant.ui.screen.home.HomeScreenState
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.rememberPagerState
 
@@ -14,12 +15,13 @@ internal fun HomeScreen(
     nacController: NavController,
     viewModel: HomeViewModel = viewModel()
 ) {
-    val pagerState = rememberPagerState(mockPlanList.size)
+    val state: HomeScreenState by viewModel.state.observeAsState(HomeScreenState.generateInitialState())
+    val pagerState = rememberPagerState(state.plans.size)
 
     HomeContent(
         screenName = String(),
         onClick = {},
         pagerState = pagerState,
-        mockPlanList,
+        planList = state.plans,
     )
 }
